b28e339e5aa2ab9acc534287343b8a7bd1700fa4
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-08-10 23:39:00 +0100

    createArea Component

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -6,65 +6,65 @@ import AddIcon from "@mui/icons-material/Add";
 
 
 const CreateArea = (props) => {
-    const[isExpanded, setIsExpanded]=useState(false);
+const[isExpanded, setIsExpanded]=useState(false);
 
-  const [note, setNote] = useState({
+const [note, setNote] = useState({
+title: "",
+content: "",
+});
+
+function submitNote(e) {
+e.preventDefault();
+props.onAdd(note); // Assuming you want to call the onAdd function passed as a prop
+
+setNote({
     title: "",
     content: "",
-  });
+});
+}
 
-  function submitNote(e) {
-    e.preventDefault();
-    props.onAdd(note); // Assuming you want to call the onAdd function passed as a prop
+function handleChange(e) {
+const { name, value } = e.target;
+setNote((prevNote) => ({
+    ...prevNote,
+    [name]: value,
+}));
+}
 
-    setNote({
-      title: "",
-      content: "",
-    });
-  }
+function Expand(){
+setIsExpanded(true);
+}
 
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setNote((prevNote) => ({
-      ...prevNote,
-      [name]: value,
-    }));
-  }
+return (
+<div>
+    <form className="create-note">
+    {isExpanded &&(
+        <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        onChange={handleChange}
+        value={note.title}
+    />
+    )}
 
-  function Expand(){
-    setIsExpanded(true);
-  }
-
-  return (
-    <div>
-      <form className="create-note">
-      {isExpanded &&(
-           <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          onChange={handleChange}
-          value={note.title}
-        />
-      )}
-
-     
-        <textarea
-          name="content"
-          placeholder="Take a note..."
-          rows={isExpanded ? 3 : 1}
-          onClick={Expand}
+    
+    <textarea
+        name="content"
+        placeholder="Take a note..."
+        rows={isExpanded ? 3 : 1}
+        onClick={Expand}
 
 
-          onChange={handleChange}
-          value={note.content}
-        />
-        <Fab onClick={submitNote}>
-          <AddIcon />
-        </Fab>
-      </form>
-    </div>
-  );
+        onChange={handleChange}
+        value={note.content}
+    />
+    <Fab onClick={submitNote}>
+        <AddIcon />
+    </Fab>
+    </form>
+</div>
+);
 };
 
 export default CreateArea;
