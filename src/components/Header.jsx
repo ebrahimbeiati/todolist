b48875867f5d418ac0f81c396fd455422b46448f
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-08-10 23:38:31 +0100

    create Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import GradingIcon from "@mui/icons-material/Grading";
 const Header = () => {
-  return (
-   <header>
-    <h1>
-        <GradingIcon />
-        To Do List 
-    </h1>
-   </header>
-  )
+return (
+<header>
+<h1>
+    <GradingIcon />
+    To Do List 
+</h1>
+</header>
+)
 }
 
 export default Header;
