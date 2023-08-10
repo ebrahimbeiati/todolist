62a18c1d180eedc5e759928e9624e53f8b76f531
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-08-10 23:39:57 +0100

    Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,55 +4,55 @@ import EditIcon from "@mui/icons-material/Edit";
 import SaveAsIcon from "@mui/icons-material/SaveAs";
 
 const Note = ({
-  id,
-  title: initialTitle,
-  content: initialContent,
-  onDelete,
+id,
+title: initialTitle,
+content: initialContent,
+onDelete,
 }) => {
-  const [title, setTitle] = useState(initialTitle);
-  const [content, setContent] = useState(initialContent);
-  const [isEditing, setIsEditing] = useState(false);
+const [title, setTitle] = useState(initialTitle);
+const [content, setContent] = useState(initialContent);
+const [isEditing, setIsEditing] = useState(false);
 
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
+const handleEdit = () => {
+setIsEditing(true);
+};
 
-  const handleSave = () => {
-    // You can add logic here to save the edited content, if needed
-    setIsEditing(false);
-  };
+const handleSave = () => {
+// You can add logic here to save the edited content, if needed
+setIsEditing(false);
+};
 
-  return (
-    <div className={`note ${isEditing ? "editing" : ""}`}>
-      {isEditing ? (
-        <>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <button onClick={handleSave}>
-            <SaveAsIcon />
-          </button>
-        </>
-      ) : (
-        <>
-          <h1>{title}</h1>
-          <p>{content}</p>
-          <button className="edit-button" onClick={handleEdit}>
-            <EditIcon />
-          </button>
-          <button onClick={onDelete}>
-            <DeleteIcon />
-          </button>
-        </>
-      )}
-    </div>
-  );
+return (
+<div className={`note ${isEditing ? "editing" : ""}`}>
+    {isEditing ? (
+    <>
+        <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        />
+        <button onClick={handleSave}>
+        <SaveAsIcon />
+        </button>
+    </>
+    ) : (
+    <>
+        <h1>{title}</h1>
+        <p>{content}</p>
+        <button className="edit-button" onClick={handleEdit}>
+        <EditIcon />
+        </button>
+        <button onClick={onDelete}>
+        <DeleteIcon />
+        </button>
+    </>
+    )}
+</div>
+);
 };
 
 export default Note;
